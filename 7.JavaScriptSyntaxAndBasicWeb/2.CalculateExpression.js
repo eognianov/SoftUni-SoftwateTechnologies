81a28369946a3c161cabd1302ebb55478db6fc52
eognianov <emilian_89@abv.bg>
2017-11-11 16:24:25 +0200

    js task 3

--- 7.JavaScriptSyntaxAndBasicWeb/2.CalculateExpression.js
+++ 7.JavaScriptSyntaxAndBasicWeb/2.CalculateExpression.js
@@ -1,3 +1,5 @@
-let sum = ((30+25) * 1/3 *(35-14-12));
-let expr =sum**2;
-console.log(expr);
+function solution() {
+    let sum = ((30+25) * 1/3 *(35-14-12));
+    let expr =sum**2;
+    console.log(expr);
+}
