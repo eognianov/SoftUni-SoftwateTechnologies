29e742c54a4a5572ea1adf54c76f41f17edd040f
eognianov <emilian_89@abv.bg>
2017-12-01 16:41:34 +0200

    dasda

--- 19.JavaBasicSyntaxLab/src/SumsByTown.java
+++ 19.JavaBasicSyntaxLab/src/SumsByTown.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class SumsByTown {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        TreeMap<String,Double> sumsByTown = new TreeMap<>();
+        int n = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] tokens = scan.nextLine().split("\\|");
+            String town = tokens[0].trim();
+            double income = Double.parseDouble(tokens[1].trim());
+            if(sumsByTown.containsKey(town)){
+                sumsByTown.put(town,sumsByTown.get(town)+income);
+            }else{
+                sumsByTown.put(town,income);
+            }
+        }
+        for (String key: sumsByTown.keySet()){
+            System.out.println(key + " -> " + sumsByTown.get(key));
+        }
+
+    }
+
+
 }
